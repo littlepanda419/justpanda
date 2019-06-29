774fccca825c89a35f23a15e40598ca3dc56985a
pandayoooo <yoyoandjoy419@gmail.com>
2019-06-29 19:51:51 +0800

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const discord = require("discord.js");
-const bot = new discord.Client();
-
-bot.on('message', message => {
-    if (message.content === 'ping') {
-       message.reply('pong');
-       }
-});
-bot.login(process.env.token);
