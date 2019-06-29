c040868e27d883e1fe6250cfbe595e473d28eb14
pandayoooo <yoyoandjoy419@gmail.com>
2019-06-29 19:21:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
+const PREFIX ="p.:
 
  
 
 client.on('ready', () => {
-
     console.log('I am ready!');
-
 });
 
  
@@ -26,4 +24,4 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 
-client.login(process.env.NTk0MzczMDI0ODc5NDc2NzM3.XRceuw.Jzj-o0b3OD0fnU2Eke1liDLC4_c);//where BOT_TOKEN is the token of our bot
+client.login(process.env.NTk0MzczMDI0ODc5NDc2NzM3.XRdCUg.pE_IC4Q839cyQExNGpd3yZ2dWHM);//where BOT_TOKEN is the token of our bot
