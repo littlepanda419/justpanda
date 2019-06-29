b09337c19179149db9e9175d8f057566937ad532
pandayoooo <yoyoandjoy419@gmail.com>
2019-06-29 19:40:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const PREFIX ="p.:
-
- 
-
-client.on('ready', () => {
-    console.log('I am ready!');
-});
-
- 
 
 client.on('message', message => {
 
@@ -19,9 +10,4 @@ client.on('message', message => {
        }
 
 });
-
- 
-
-// THIS  MUST  BE  THIS  WAY
-
-client.login(NTk0MzczMDI0ODc5NDc2NzM3.XRdLaA.rm6-YR5zZ8pMKBUlIb1-I-BaFmA);//where BOT_TOKEN is the token of our bot
+client.login('NTk0MzczMDI0ODc5NDc2NzM3.XRdLaA.rm6-YR5zZ8pMKBUlIb1-I-BaFmA');//where BOT_TOKEN is the token of our bot
