7552b43a84f487ae28563a4dd27bc32aa678aa08
pandayoooo <yoyoandjoy419@gmail.com>
2019-06-29 19:56:13 +0800

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+	console.log('i am ready!');
+});
+
+client.on('message', message =>{
+	if(message.content ==='ping' {
+			message.reply('pong');
+	}
+});
+
+//
+client.login(process.env.BOT_TOKEN);		
