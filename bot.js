1a5434fc4f3db35479b77d1ad7e1c296d079704d
pandayoooo <yoyoandjoy419@gmail.com>
2019-06-30 22:03:55 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ client.on('ready', () =>
 client.on('message', (message) =>{
 	
 	var d = new Date();
-	var h = d.getHours();
+	var h = d.getHours()+8;
 	var m = d.getMinutes();
 	var s = d.getSeconds();
 	
