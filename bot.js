09402217a97d122f098e3164e6ad3b9119ea91f2
pandayoooo <yoyoandjoy419@gmail.com>
2019-06-30 09:31:41 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,15 +6,7 @@ client.on('ready', () =>
 	console.log('i am ready!');
 });
 
-client.on('message', (message) =>{
-	if(message.content ==="彥宇") 
-	{
-		message.channel.send("猴爆");
-	}
-	if(message.content ==="你是不是孫文浩") 
-	{
-		message.channel.send("你才死胖呆");
-	}
+client.on('message', (message) =>{	
 	if(message.content ==="歐姆定律") 
 	{
 		message.reply("V=IR , I=V/R , R= V/I");
