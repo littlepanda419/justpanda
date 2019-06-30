bd5f2516a35b9621fabb25892fd29fbcde789f26
pandayoooo <yoyoandjoy419@gmail.com>
2019-06-30 19:54:57 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,39 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+
 client.on('ready', () => 
 	{
 	console.log("機器人已上線");
 });
 
 client.on('message', (message) =>{	
+	Textchannel.typing===(1);
+	var time = today.getHours();
+
+	if(message.content ==="test")
+	{
+		client.channels.get("594119720022573076").send("9點了 該吃消夜嚕");
+	}
+
+	
+	if(time === 21 )
+	{
+		client.channels.get("594119720022573076").send("9點了 該吃消夜嚕");
+	}
+	if(time === 6 )
+	{
+		client.channels.get("594119720022573076").send("6點了 快起床阿幹");
+	}
+	if(time === 12 )
+	{
+		client.channels.get("594119720022573076").send("12點了 該吃午餐嚕");
+	}
+	if(time === 18 )
+	{
+		client.channels.get("594119720022573076").send("6點了 該吃晚餐嚕");
+	}
+
 	if(message.content ==="歐姆定律") 
 	{
 		message.reply("V=IR , I=V/R , R= V/I");
