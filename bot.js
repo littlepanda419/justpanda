34d311c51b928d82faf78cbf645ec7c752622c79
pandayoooo <yoyoandjoy419@gmail.com>
2019-06-30 23:39:02 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,9 @@ var s = d.getSeconds();
 
 client.on('ready', () => 
 	{
-var generalChannel = client.channels.get("594119720022573076") ;
+	const kcl = new Discord.Attachment('https://cdn.discordapp.com/attachments/594119720022573076/594555557709611033/unknown.png')
+	const kvl = new Discord.Attachment('https://cdn.discordapp.com/attachments/594119720022573076/594556625155784724/unknown.png')
+	var generalChannel = client.channels.get("594119720022573076") ;
 	
 	console.log("機器人已上線");
 	generalChannel.send(" <@324536397803290626> bot已在 "+h+":"+m+":"+s+ " 時運作。") ;
@@ -67,11 +69,11 @@ client.on('message', (message) =>{
 	}
 	if(message.content ==="KVL") 
 	{
-		message.reply( "  " + "https://cdn.discordapp.com/attachments/594119720022573076/594556625155784724/unknown.png");
+		message.reply(KVL);
 	}
 	if(message.content ==="KCL") 
 	{
-		message.reply( "  " + "https://cdn.discordapp.com/attachments/594119720022573076/594555557709611033/unknown.png");
+		message.reply(KCL);
 	}
 });
 client.login(process.env.BOT_TOKEN);		
