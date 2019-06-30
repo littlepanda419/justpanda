68cde98668d759dbe888cf3cbcd84994206201fe
pandayoooo <yoyoandjoy419@gmail.com>
2019-06-30 22:04:12 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ client.on('message', (message) =>{
 	{
  	   generalChannel.send(h+" "+m+" "+s) ;
 	}
-	if(h === 21 && m===53 && s<=10)
+	if(h === 22 && m===6 && s<=10)
 	{
 		generalChannel.send("9點了 該吃消夜嚕")  ;
 	}
