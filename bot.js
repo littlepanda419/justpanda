fb63aa84a6d753942f96050759e792eadb2df91d
pandayoooo <yoyoandjoy419@gmail.com>
2019-06-30 22:26:25 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,12 +4,17 @@ const client = new Discord.Client();
 
 client.on('ready', () => 
 	{
+	var d = new Date();
+	var h = d.getHours()+8;
+	var m = d.getMinutes();
+	var s = d.getSeconds();
+
 	console.log("機器人已上線");
+	generalChannel.send(" <@324536397803290626> 熊貓已在 "+h+":"+m+":"+s+ " 時運作。") ;
 });
 
 client.on('message', (message) =>{
 	
-	var d = new Date();
 	var h = d.getHours()+8;
 	var m = d.getMinutes();
 	var s = d.getSeconds();
