3760239d23265764346bb8229378de086cb9cf32
pandayoooo <yoyoandjoy419@gmail.com>
2019-07-01 19:34:39 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,54 +16,13 @@ client.on('ready', () =>{
   
 	var generalChannel = client.channels.get("594119720022573076") ;		
 	console.log("機器人已上線");
-	generalChannel.send(" <@324536397803290626> bot已在 "+h+":"+m+":"+s+ " 時運作。");
-  
-   });
+	generalChannel.send(" <@324536397803290626> bot已在 "+h+":"+m+":"+s+ " 時運作。");  
 	// Set the client user's status
 	client.user.setStatus('idle');
 	client.user.setActivity('code', { type: 'PLAYING' });
           
     });
-	/*console.log(get_current_time());
-  	returntime = (4-today.getMinutes())*60*1000+(60-today.getSeconds())*1000;
-  	if(today.getHours()<12)
-    {
-        returntime += (11-today.getHours())*1000*60*60;
-    }
-    else if(today.getHours()<18)
-    {
-        returntime += (14-today.getHours())*1000*60*60;
-    }
-    var gc = client.channels.get("594119720022573076");
-    client.setTimeout(function(){gc.send("<@!324536397803290626>吃午餐啦");},returntime);*/
 
-	
-
-/*
-	
-	setTimeout(function(){ // in leftToEight() milliseconds run this:
-        var dayMillseconds = 1000 * 60 * 60 * 24;
-  	setInterval(function(){ // repeat this every 24 hours
-            sendNightNight();
-        }, dayMillseconds)
-    }, leftToTen())
-	
-	
-});
-
-function leftToSix(){
-	var d = new Date();	
-    return (-d + d.setHours(6,0,0,0));
-}
-
-function sendMorning(){
-    var guild = client.guilds.get('450975130387218454');
-    if(guild && guild.channels.get('594119720022573076')){
-        guild.channels.get('594119720022573076').send("6點了 快起床阿幹 <@554654697261105180> ");
-    }
-
-}
-*/
 function addZero(i) {
 	if (i < 10) {
 	  i = "0" + i;
