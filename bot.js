bbd631e87b3138134e065940d054d7c69a80adcc
pandayoooo <yoyoandjoy419@gmail.com>
2019-07-01 14:22:42 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,42 +12,36 @@ function Hex() {
     generalChannel.send(" <@551290295078223885> AS U WISH") ;
 }
 client.on('ready', () => 
-	{	
-	var generalChannel = client.channels.get("594119720022573076") ;
-		
+	{		
+	var generalChannel = client.channels.get("594119720022573076") ;		
 	console.log("機器人已上線");
 	generalChannel.send(" <@324536397803290626> bot已在 "+h+":"+m+":"+s+ " 時運作。") ;
-	Hex();
 	client.user.setPresence({
         game: { 
             name: 'code',
             type: 'PLAYING'
         },
         status: 'idle'
+		
+	
+	console.log(get_current_time());
+  	returntime = (24-today.getMinutes())*60*1000+(0-today.getSeconds())*1000;
+  	if(today.getHours()<12)
+	{
+		returntime += (11-today.getHours())*1000*60*60;
+	}
+	    else if(today.getHours()<18)
+	{
+		returntime += (14-today.getHours())*1000*60*60;
+	}
+	client.setTimeout(function(){gc.send("<@!324536397803290626>吃午餐啦");},returntime);
+
+
+	
     })
 
 
-	setTimeout(function(){ // in leftToEight() milliseconds run this:
-        var dayMillseconds = 1000 * 60 * 60 * 24;
-        setInterval(function(){ // repeat this every 24 hours
-            sendMorning();
-        }, dayMillseconds)
-    }, leftToSix())
-
-	setTimeout(function(){ // in leftToEight() milliseconds run this:
-        var dayMillseconds = 1000 * 60 * 60 * 24;
-	setInterval(function(){ // repeat this every 24 hours
-            sendNoon();
-        }, dayMillseconds)
-    }, leftToTwelve())
-
-	setTimeout(function(){ // in leftToEight() milliseconds run this:
-        var dayMillseconds = 1000 * 60 * 60 * 24;
-	setInterval(function(){ // repeat this every 24 hours
-            sendNight();
-        }, dayMillseconds)
-    }, leftToEight())
-    
+	
 	setTimeout(function(){ // in leftToEight() milliseconds run this:
         var dayMillseconds = 1000 * 60 * 60 * 24;
   	setInterval(function(){ // repeat this every 24 hours
@@ -71,42 +65,6 @@ function sendMorning(){
 
 }
 
-function leftToTwelve(){
-	var d = new Date();	
-    return (-d + d.setHours(12,0,0,0));
-}
-function sendNoon(){
-    var guild = client.guilds.get('450975130387218454');
-    if(guild && guild.channels.get('594119720022573076')){
-        guild.channels.get('594119720022573076').send("12點了 該吃午餐嚕 <@554654697261105180> ");
-    }
-
-}
-
-function leftToEight(){
-	var d = new Date();	
-    return (-d + d.setHours(18,0,0,0));
-}
-function sendNight(){
-    var guild = client.guilds.get('450975130387218454');
-    if(guild && guild.channels.get('594119720022573076')){
-        guild.channels.get('594119720022573076').send("6點了 該吃晚餐嚕 <@554654697261105180> ");
-    }
-
-}
-
-function leftToTen(){
-	var d = new Date();	
-    return (-d + d.setHours(20,0,0,0));
-}
-function sendNightNight(){
-    var guild = client.guilds.get('450975130387218454');
-    if(guild && guild.channels.get('594119720022573076')){
-        guild.channels.get('594119720022573076').send("10點了 該吃消夜嚕 <@554654697261105180> ");
-    }
-
-}
-
 
 
 client.on('message', (message) =>{
@@ -123,25 +81,7 @@ client.on('message', (message) =>{
 	if(message.content.toUpperCase()==="TEST") 
 	{
  	   generalChannel.send(h+":"+m+":"+s) ;
-	}
-	
-	/*if(h === 21 && m===0 && s<=2)
-	{
-		generalChannel.send("9點了 該吃消夜嚕")  ;
-	}
-	if(h === 6  && m===0 && s<=2)
-	{
-		generalChannel.send("6點了 快起床阿幹")  ;
-	}
-	if(h === 12 && m===0 && s<=2)
-	{
-		generalChannel.send("12點了 該吃午餐嚕")  ;
-	}
-	if(h === 18 && m===0 && s<=2)
-	{
-		generalChannel.send("6點了 該吃晚餐嚕")  ;
-	}*/
-	
+	}		
 	if(message.content ==="歐姆定律") 
 	{
 		message.reply("V=IR , I=V/R , R= V/I");
