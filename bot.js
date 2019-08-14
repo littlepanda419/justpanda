68f21f1f42b6801d9846a275420c2efa3b1c3737
pandayoooo <yoyoandjoy419@gmail.com>
2019-08-14 22:43:07 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -61,14 +61,14 @@ client.on('message', (message) =>{
  	   generalChannel.send(h+":"+m+":"+s) ;
 	}
 	
-	if (message.content === "欸欸欸你過來一下"||message.content ==="欸欸欸你進來一下"||message.content ==="過來一下"||message.content ==="pandain")
+	if (message.content === "欸欸欸你過來一下"||message.content ==="欸欸欸你進來一下"||message.content ==="過來一下"||message.content.toUpperCase()==="PANDAIN")
 	{
 		const musicchannel = message.member.voiceChannel;
 		musicchannel.join();
 		message.channel.send("已加入語音");	
 	}
 
-	if (message.content === "滾啦幹"||message.content ==="pandaout")
+	if (message.content === "滾啦幹"||message.content.toUpperCase()==="PANDAOUT")
 	{		
 		const musicchannel = message.member.voiceChannel;
 		musicchannel.leave();
