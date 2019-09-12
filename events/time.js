e9f8b1ba1e829d4de6bb1934fbe37e297e05f806
pandayoooo <yoyoandjoy419@gmail.com>
2019-09-13 00:56:15 +0800

    Add files via upload

--- events/time.js
+++ events/time.js
@@ -1,7 +1,16 @@
 const Discord = require('discord.js')
 module.exports = (msg,client) =>
 
-{  var d = new Date();	
+{
+	function addZero(i) 
+	{
+	if (i < 10) {
+	  i = "0" + i;
+	}
+	return i;
+  }
+
+var d = new Date();	
 var h = addZero(d.getHours()+8);
 var m = addZero(d.getMinutes());
 var s = addZero(d.getSeconds());
