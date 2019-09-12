07905a9ee470d39dbe6cd73c69c85e086018126c
pandayoooo <yoyoandjoy419@gmail.com>
2019-09-13 00:51:35 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,19 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const addzero = require("./events/addzero.js");
 const emote = require("./events/emote.js");
 const msg = require("./events/msg.js");
 const music = require("./events/music.js");
 const time = require("./events/time.js");
+
+function addZero(i) 
+	{
+	if (i < 10) {
+	  i = "0" + i;
+	}
+	return i;
+  }
+
+
 
 
 
@@ -21,13 +30,18 @@ const time = require("./events/time.js");
 
 client.on('ready', () =>{
 	
-	var generalChannel = new client.channels.get("594119720022573076") ;
-	console.log("機器人已上線");
-	generalChannel.send(" <@324536397803290626> bot已在 "+h+":"+m+":"+s+ " 時開始吃竹子。");  
+	var d = new Date();	
+	var h = addZero(d.getHours()+8);
+	var m = addZero(d.getMinutes());
+	var s = addZero(d.getSeconds());
+
+	var generalChannel =  client.channels.get("594119720022573076") ;		
+	const musicchannel = "506108715720769536";	  
 	client.user.setStatus('idle');
-	client.user.setActivity("扣ㄉ", { type: 'PLAYING' });
-	const musicchannel = "506108715720769536";	
+	client.user.setActivity("扣ㄉ", { type: 'PLAYING' });	
+	generalChannel.send(" <@324536397803290626> bot已在 "+h+":"+m+":"+s+ " 時開始吃竹子。");
 	musicchannel.join();
+	console.log("機器人已上線");
 });
   
 client.on('message', (message) =>{
@@ -36,7 +50,6 @@ client.on('message', (message) =>{
 		time(msg,client);
 		music(msg,client);
 		emote(msg,client);
-		addzero(msg,client);
 		msg(msg,client);
 
 });
