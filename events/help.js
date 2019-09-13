cb2c75b3268f773b460ff82cdc53282648be5d36
pandayoooo <yoyoandjoy419@gmail.com>
2019-09-13 12:12:50 +0800

    Update help.js

--- events/help.js
+++ events/help.js
@@ -12,15 +12,15 @@ module.exports = (msg,client) =>{
     if(msg.content.toLocaleLowerCase()=="help")
     {
         var embed = new Discord.RichEmbed()
-            embed.setColor(0xFFFF00);
+            .setColor(0xFFFF00);
             embed.addField("**指令說明**","快點把指令背起啦來幹");
-            embed.addField("**1.文字回應**","派  胎死腹中  歐姆定律  怕  KVL  KCL  自走其  ");
-            embed.addField("**2.語音**","pandain  pandaout");
-            embed.addField("**3.音樂**","");
-            embed.addField("**4.管理**","");
-            embed.addField("**5.算數學**","");
-            embed.addField("**6.幫你估狗**","");
-            embed.addField("**7.我還想不到不過會有**","");
+            embed.addField("**1.文字回應**","派  胎死腹中  歐姆定律  怕  KVL  KCL  自走其  ",true);
+            embed.addField("**2.語音**","pandain  pandaout",true);
+            embed.addField("**3.音樂**","123",true);
+            embed.addField("**4.管理**","123",true);
+            embed.addField("**5.算數學**","123",true);
+            embed.addField("**6.幫你估狗**","123",true);
+            embed.addField("**7.我還想不到不過會有**","123",true);
             message.author.send({embed});
             message.channel.send("好喔!我已經把指令說明寄給你了");
         
