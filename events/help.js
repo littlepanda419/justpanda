a6db2418e13ef22ceb499926fe46ec48097fb252
pandayoooo <yoyoandjoy419@gmail.com>
2019-09-13 12:21:42 +0800

    Update help.js

--- events/help.js
+++ events/help.js
@@ -11,17 +11,17 @@ module.exports = (msg,client) =>{
     }
     if(msg.content.toLocaleLowerCase()=="help")
     {
-        var embed = new Discord.RichEmbed()
-            .setColor(0xFFFF00);
-            embed.addField("**指令說明**","快點把指令背起啦來幹");
-            embed.addField("**1.文字回應**","派  胎死腹中  歐姆定律  怕  KVL  KCL  自走其  ",true);
-            embed.addField("**2.語音**","pandain  pandaout",true);
-            embed.addField("**3.音樂**","123",true);
-            embed.addField("**4.管理**","123",true);
-            embed.addField("**5.算數學**","123",true);
-            embed.addField("**6.幫你估狗**","123",true);
-            embed.addField("**7.我還想不到不過會有**","123",true);
-            message.author.send({embed});
+        var help = new Discord.RichEmbed()
+            .setColor(0xFFFF00)
+            .addField("**指令說明**","快點把指令背起啦來幹")
+            .addField("**1.文字回應**","派  胎死腹中  歐姆定律  怕  KVL  KCL  自走其  ",true)
+            .addField("**2.語音**","pandain  pandaout",true)
+            .addField("**3.音樂**","123",true)
+            .addField("**4.管理**","123",true)
+            .addField("**5.算數學**","123",true)
+            .addField("**6.幫你估狗**","123",true)
+            .addField("**7.我還想不到不過會有**","123",true)
+            message.author.send(help);
             message.channel.send("好喔!我已經把指令說明寄給你了");
         
     }
