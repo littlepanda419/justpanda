d371e77e19ae922214b3a336d30b04a99b8a3759
pandayoooo <yoyoandjoy419@gmail.com>
2019-09-13 13:52:11 +0800

    Update msg.js

--- events/msg.js
+++ events/msg.js
@@ -30,35 +30,6 @@ module.exports = ('message', (message) =>
 	{
  	   message.channel.send(h+":"+m+":"+s) ;
 	}
-	
-	if (message.content === "欸欸欸你過來一下"||message.content ==="欸你過來一下"||message.content ==="欸你進來一下"
-	||message.content ==="欸欸欸你進來一下"||message.content ==="過來一下"||message.content.toUpperCase()==="PANDAIN")
-	{
-		const musicchannel = message.member.voiceChannel;
-		try {
-			musicchannel.join();
-		message.channel.send("已加入語音");	
-		message.react("🐼");
-		} catch (error) {
-			message.channel.send("進不去啦幹");
-			emoji(612549755502985247);
-			message.react(`${emoji(/*表情ID*/ )}`);
-		}
-	}
-	if (message.content === "滾啦幹"||message.content.toUpperCase()==="PANDAOUT")
-	{
-		const musicchannel = message.member.voiceChannel;
-		try {
-		musicchannel.leave();
-		message.channel.send("已離開語音");
-		message.react("🐼");	
-		} catch (error) {
-		message.channel.send("08偏不要滾");
-		message.react("🐼");
-		}
-	}
-	
-	
 
 	if(message.content.includes("胎死腹中"))
 	{
@@ -72,7 +43,6 @@ module.exports = ('message', (message) =>
 				message.react("🐼");
 			});
 		}
-
 
 	if(message.content ==="歐姆定律") 
 	{
