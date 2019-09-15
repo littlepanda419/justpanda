c4df846d22ee3e98a3d09c31572e7c4044e0dd9c
pandayoooo <yoyoandjoy419@gmail.com>
2019-09-15 09:57:27 +0800

    Update music.js

--- events/music.js
+++ events/music.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 
-module.exports =(message,client) =>
+module.exports =('message', (message) =>
 {
 	if (message.content.toUpperCase()==="PANDAIN")
 	{
