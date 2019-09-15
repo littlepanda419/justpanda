a4b14e96e438f6698f8b4afc1125c4ca243e2efb
pandayoooo <yoyoandjoy419@gmail.com>
2019-09-15 09:54:11 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -58,7 +58,7 @@ client.on('message', (message) =>{
 	return;		
 		time(message,client);
 		help(message,client);
-		music(client,message,args,ops);
+		music(message,client);
 		emote(message,client);
 		msg(message,client);
 		play(client,message,args,ops);
