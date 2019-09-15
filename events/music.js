20c8a45fe1bff11cd7221af6eeb22377a13dc689
pandayoooo <yoyoandjoy419@gmail.com>
2019-09-15 09:39:16 +0800

    Update music.js

--- events/music.js
+++ events/music.js
@@ -1,17 +1,24 @@
 const Discord = require('discord.js');
-//const fs = require('fs');
 const ytdl = require('ytdl-core');
 const client = new Discord.Client();
-
-
-
-
-	  
-	  
-module.exports =('message', (message) =>
+/*
+function play(connection, message)
 {
-	if (message.content === "欸欸欸你過來一下"||message.content ==="欸你過來一下"||message.content ==="欸你進來一下"
-	||message.content ==="欸欸欸你進來一下"||message.content ==="過來一下"||message.content.toUpperCase()==="PANDAIN")
+	var server =servers[message.guild.id];
+	server.dispatcher = connection.playStream(ytdl(sever.queue[0], {filter:"audioonly"}));
+	server.queue.shift();
+	server.dispatcher.on("end",function(){
+		if(server.queue[0])	 
+		{
+		play(connection,message);
+	}
+	});
+}
+*/
+
+module.exports =(client,message,args,ops) =>
+{
+	if (message.content.toUpperCase()==="PANDAIN")
 	{
 		const musicchannel = message.member.voiceChannel;
 		try {
@@ -25,9 +32,22 @@ module.exports =('message', (message) =>
 			message.react(`${emoji(/*表情ID*/ )}`);
 		}
 	}
-	
 
-	if (message.content === "滾啦幹"||message.content.toUpperCase()==="PANDAOUT")
+	if (message.content.startsWith(play))
+	{
+		if (!message.member.voiceChannel) return message.member.channel.send('你要先進入語音');
+		if (member.guild.me.voiceChannel) return message.member.channel.send('我在別人家了');
+		if (!args[0]) return message.channel.send('給我連結啦幹');
+		let validate = await ytdl.validateURL(args[0]); //validate info 
+		if(!validate) return message.channel.send ('請在指令後輸入有效**網址**');
+		let info = await ytdl.getInfo(args[0]);
+		let connection = await message.member.voiceChannel.join();
+		let dispatcher = await connection.play(ytdl(args[0], {filter : 'audioonly'}));
+		message.channel.send(`Now playing : ${info.title} `);
+		client.user.setActivity(`${info.title}`, { type: 'PLAYING' });	
+
+	}
+	if (message.content.toUpperCase()==="PANDAOUT")
 	{
 		const musicchannel = message.member.voiceChannel;
 		try {
