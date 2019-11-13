7e392e848d4bbc9e1c34ef1f7a7c30f88b63dc10
pandayoooo <yoyoandjoy419@gmail.com>
2019-11-13 14:29:11 +0800

    Update music.js

--- events/music.js
+++ events/music.js
@@ -4,7 +4,7 @@ const ytdl = require('ytdl-core');
 
 const client = new Client({ disableEveryone: true });
 const {PREFIX, GOOGLE_API_KEY } = require('../config');
-const youtube = new YouTube(process.env.GOOGLE_API_KEY);
+const youtube = new YouTube(GOOGLE_API_KEY);
 
 const queue = new Map();
 
