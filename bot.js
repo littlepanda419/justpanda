11f802f5b9e8f48e959f3d0b36033334185bf1c7
pandayoooo <yoyoandjoy419@gmail.com>
2019-11-13 14:42:07 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,6 @@ const time = require("./events/time.js");
 const help = require("./events/help.js");
 
 const client = new Discord.Client();
-// "ffmpeg": "4.2.1", //https://ffmpeg.zeranoe.com/builds/
 const { token, PREFIX, GOOGLE_API_KEY } = require('./config');
 const youtube = new YouTube(GOOGLE_API_KEY);
 
