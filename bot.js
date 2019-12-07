7c6a1b6063da962265a8a28788005a92ac5aae81
pandayoooo <yoyoandjoy419@gmail.com>
2019-12-07 18:43:27 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,82 +1,125 @@
-const Discord = require('discord.js');
-const ytdl = require('ytdl-core');
-const YouTube = require('simple-youtube-api');
-const emote = require("./events/emote.js");
-const msg = require("./events/msg.js");
-//const pic = require("./events/pic.js");
-const music = require("./events/music.js");
-const time = require("./events/time.js");
-const help = require("./events/help.js");
-const say = require("./events/say.js");
-const { google } = require('googleapis');
-const ignore = require('ignore-errors');
-var colors = require('colors');
-const crypto = require('crypto');
-const { GoogleToken } = require('gtoken');
-const jwt = require('jsonwebtoken');
-const {auth} = require('google-auth-library');
+var fs = require('fs');
+var readline = require('readline');
+var {google} = require('googleapis');
+var OAuth2 = google.auth.OAuth2;
 
-
-const client = new Discord.Client();
-const { token, PREFIX, GOOGLE_API_KEY } = require('./config');
-const youtube = new YouTube(GOOGLE_API_KEY);
-
-
-
-
-
-var generalChannel =  client.channels.get("594119720022573076");
-function addZero(i) 
-	{
-	if (i < 10) {
-	  i = "0" + i;
-	}
-	return i;
+// If modifying these scopes, delete your previously saved credentials
+// at ~/.credentials/youtube-nodejs-quickstart.json
+var SCOPES = ['https://www.googleapis.com/auth/youtube'];
+var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
+    process.env.USERPROFILE) + '/.credentials/';
+var TOKEN_PATH = TOKEN_DIR + 'youtube-nodejs-quickstart.json';
+    //console.log(TOKEN_PATH);
+// Load client secrets from a local file.
+fs.readFile('client_secret.json', function processClientSecrets(err, content) {
+  if (err) {
+    console.log('Error loading client secret file: ' + err);
+    return;
   }
-
-client.on('warn', console.warn);
-client.on('error', console.error);
-client.on('diconnect', () => generalChannel.send("08被來捆阿"));
-client.on('ready', () =>{
-	console.time('start');
-	var d = new Date();	
-	var h = addZero(d.getUTCHours()+8); if (h>=24)	h = "0"+(h-24);
-	var m = addZero(d.getMinutes());
-	var s = addZero(d.getSeconds());
-	var generalChannel =  client.channels.get("594119720022573076");
-	var Myinfo = new Discord.RichEmbed()
-		.addField("蛤","肏零呆蛤沙小",true)
-		.addField("我在幹嘛","我誰",true)
-		.setColor(0xFFFF00)
-		.setThumbnail("https://qpa.tw/wp-content/uploads/2019/05/%E7%86%8A%E8%B2%93-1.jpg")
-		.setFooter("阿這麼小你也要看");
-	generalChannel.send(Myinfo);	 //洗頻大師
-	client.user.setStatus('idle');
-	client.user.setActivity("扣ㄉ", { type: 'PLAYING' });		
-	generalChannel.send(" <@324536397803290626> bot已在 "+h+":"+m+":"+s+ " 時開始吃竹子。");  //洗頻大師
-
-	var musicchannel = client.channels.get("506108715720769536");
-	try {
-	//	musicchannel.join();
-	//	generalChannel.send("已加入語音");
-	} catch (error) {
-		generalChannel.send("進不去啦幹");		
-	}
-	console.log("%s\x1b[33m",colors.rainbow ("機器人已上線"));
-	console.timeEnd('start');
+  // Authorize a client with the loaded credentials, then call the YouTube API.
+  authorize(JSON.parse(content), getChannel);
 });
-  
 
-client.on('message', (message) =>{
-	if(message.author==client.user)   
-	return;		
-		time(message,client);
-		help(message,client);
-		music(message,client);
-		emote(message,client);
-		msg(message,client);
-		//pic(message,client);
-		say(message,client);
-	});
-client.login(process.env.BOT_TOKEN);
-//client.login(token);
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ *
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback) {
+  var clientSecret = credentials.installed.client_secret;
+  var clientId = credentials.installed.client_id;
+  var redirectUrl = credentials.installed.redirect_uris[0];
+  var oauth2Client = new OAuth2(clientId, clientSecret, redirectUrl);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, function(err, token) {
+    if (err) {
+      getNewToken(oauth2Client, callback);
+    } else {
+      oauth2Client.credentials = JSON.parse(token);
+      callback(oauth2Client);
+    }
+  });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ *
+ * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback to call with the authorized
+ *     client.
+ */
+function getNewToken(oauth2Client, callback) {
+  var authUrl = oauth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES
+  });
+  console.log('Authorize this app by visiting this url: ', authUrl);
+  var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.question('Enter the code from that page here: ', function(code) {
+    rl.close();
+    oauth2Client.getToken(code, function(err, token) {
+      if (err) {
+        console.log('Error while trying to retrieve access token', err);
+        return;
+      }
+      oauth2Client.credentials = token;
+      storeToken(token);
+      callback(oauth2Client);
+    });
+  });
+}
+
+/**
+ * Store token to disk be used in later program executions.
+ *
+ * @param {Object} token The token to store to disk.
+ */
+function storeToken(token) {
+  try {
+    fs.mkdirSync(TOKEN_DIR);
+  } catch (err) {
+    if (err.code != 'EEXIST') {
+      throw err;
+    }
+  }
+  fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+    if (err) throw err;
+    console.log('Token stored to ' + TOKEN_PATH);
+  });
+}
+
+/**
+ * Lists the names and IDs of up to 10 files.
+ *
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+function getChannel(auth) {
+  var service = google.youtube('v3');
+  service.channels.list({
+    auth: auth,
+    part: 'snippet,contentDetails,statistics',
+    forUsername: 'GoogleDevelopers'
+  }, function(err, response) {
+    if (err) {
+      console.log('The API returned an error: ' + err);
+      return;
+    }
+    var channels = response.data.items;
+    if (channels.length == 0) {
+      console.log('No channel found.');
+    } else {
+      console.log('This channel\'s ID is %s. Its title is \'%s\', and ' +
+                  'it has %s views.',
+                  channels[0].id,
+                  channels[0].snippet.title,
+                  channels[0].statistics.viewCount);
+    }
+  });
+}
