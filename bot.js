9fc0b66dee3ec4e02a90ce0d72f660eee4f8e207
pandayoooo <yoyoandjoy419@gmail.com>
2019-12-07 18:10:39 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -36,7 +36,7 @@ const keys = JSON.parse(keysEnvVar);
 async function main() {
   // load the JWT or UserRefreshClient from the keys
   const client = auth.fromJSON(keys);
-  client.scopes = ['https://www.googleapis.com/auth/cloud-platform'];
+  client.scopes = ['https://www.googleapis.com/auth/youtube'];
   const url = `https://dns.googleapis.com/dns/v1/projects/${keys.project_id}`;
   const res = await client.request({url});
   console.log(res.data);
